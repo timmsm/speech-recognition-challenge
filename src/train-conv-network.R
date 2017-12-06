b6febe6020604c01f334ba9d04c356d20a3c5182
Ubuntu <ubuntu@u16.c.sixth-beaker-181800.internal>
2017-12-06 04:52:00 +0000

    Added layers and batch normalization

--- src/train-conv-network.R
+++ src/train-conv-network.R
@@ -6,26 +6,34 @@ library(tidyverse)
 
 # Generate augmented training data ----------------------------------------
 train_generator <- image_data_generator(
-  samplewise_center = TRUE, 
-  samplewise_std_normalization = TRUE, 
+  # samplewise_center = TRUE, 
+  # samplewise_std_normalization = TRUE, 
   # zca_whitening = TRUE,
-  width_shift_range = 0.1,
-  height_shift_range = 0.1,
-  zoom_range = 0.2
+  # width_shift_range = 0.1,
+  # height_shift_range = 0.1,
+  # zoom_range = 0.2
 ) %>% 
   flow_images_from_directory(
-    "data/spectrograms",  ., seed = 3521
+    "data/spectrograms",  ., seed = 4620, batch_size = 32
   )
 
 # Fit convolutional network -----------------------------------------------
 cnn <- keras_model_sequential()
 
 cnn %>% 
-  layer_conv_2d(filters = 50, kernel_size = 2, activation = "relu", input_shape = c(256, 256, 3)) %>% 
+  layer_conv_2d(filters = 16, kernel_size = 2, activation = "elu", input_shape = c(256, 256, 3)) %>% 
+  layer_conv_2d(filters = 16, kernel_size = 2, activation = "elu") %>% 
   layer_max_pooling_2d() %>% 
-  layer_conv_2d(filters = 50, kernel_size = 2, activation = "relu") %>% 
+  layer_dropout(0.2) %>% 
+  layer_conv_2d(filters = 32, kernel_size = 3, activation = "elu") %>% 
+  layer_conv_2d(filters = 32, kernel_size = 3, activation = "elu") %>% 
   layer_max_pooling_2d() %>% 
+  layer_dropout(0.2) %>% 
+  layer_conv_2d(filters = 64, kernel_size = 3, activation = "elu") %>% 
+  layer_max_pooling_2d() %>% 
+  layer_dropout(0.2) %>% 
   layer_flatten() %>% 
+  layer_batch_normalization() %>% 
   layer_dense(units = 30, activation = "softmax")
 
 cnn %>% 
