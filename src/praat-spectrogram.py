beec21b007659111b45282bce163038df6c85a30
Mark Timms <mtimms@gwu.edu>
2017-12-10 17:03:25 -0500

    Cleaned up src directory

--- src/praat-spectrogram.py
+++ src/praat-spectrogram.py
@@ -9,7 +9,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os.path
 import parselmouth
-import sys
 
 # Spectrogram functions
 def draw_spectrogram(spectrogram, dynamic_range=70):
@@ -29,39 +28,38 @@ def draw_intensity(intensity):
 
 # Create output directories
 commands = ["down", "go", "left", "no", "off", "right", "stop", "up", "yes"]
-#for c in commands:
-#    if os.path.isdir(os.path.join("../train", c)):
-#        rmtree(os.path.join("../train", c))
-#    os.makedirs(os.path.join("../train", c))
-#    
-#    if os.path.isdir(os.path.join("../validation", c)):
-#        rmtree(os.path.join("../validation", c))
-#    os.makedirs(os.path.join("../validation", c))
+for c in commands:
+    if os.path.isdir(os.path.join("../train", c)):
+        rmtree(os.path.join("../train", c))
+    os.makedirs(os.path.join("../train", c))
+    
+    if os.path.isdir(os.path.join("../validation", c)):
+        rmtree(os.path.join("../validation", c))
+    os.makedirs(os.path.join("../validation", c))
 
 # Create spectrograms
-#for c in commands:
-for wave_file in [sys.argv[1]]:
-    snd = parselmouth.Sound(wave_file)
-    snd.pre_emphasize()
-    
-    intensity = snd.to_intensity()
-    spectrogram = snd.to_spectrogram()
-    
-    if np.random.uniform(1, 100) <= 15:
-        dest = "validation"
-    else:
-        dest = "train"
-        
-    output_file = wave_file.replace("../data/train/audio/", "")\
-        .replace("wav", "png")
-    
-    plt.figure()
-    draw_spectrogram(spectrogram)
-    plt.twinx()
-    draw_intensity(intensity)
-    plt.xlim([0, 1])
-    plt.savefig(os.path.join("..", dest, output_file))
-    plt.close()
-    plt.clf()
-    plt.cla()
-    
+for c in commands:
+  for wave_file in glob.glob("../data/train/audio/%s/*.wav" % c):
+      snd = parselmouth.Sound(wave_file)
+      snd.pre_emphasize()
+      
+      intensity = snd.to_intensity()
+      spectrogram = snd.to_spectrogram()
+      
+      if np.random.uniform(1, 100) <= 15:
+          dest = "validation"
+      else:
+          dest = "train"
+          
+      output_file = wave_file.replace("../data/train/audio/", "")\
+          .replace("wav", "png")
+      
+      plt.figure()
+      draw_spectrogram(spectrogram)
+      plt.twinx()
+      draw_intensity(intensity)
+      plt.xlim([0, 1])
+      plt.savefig(os.path.join("..", dest, output_file))
+      plt.close()
+      plt.clf()
+      plt.cla()
--- src/train-conv-network.R
+++ src/train-conv-network.R
@@ -1,3 +1,5 @@
+#!/usr/bin/Rscript
+
 # Program: train-conv-network.R
 # Purpose: Train a Convolutional Network to recognize command words
 
@@ -6,22 +8,30 @@ library(tidyverse)
 
 # Generate augmented training data ----------------------------------------
 train_generator <- image_data_generator(
-  # samplewise_center = TRUE, 
-  # samplewise_std_normalization = TRUE, 
-  # zca_whitening = TRUE,
-  # width_shift_range = 0.1,
-  # height_shift_range = 0.1,
-  # zoom_range = 0.2
+  samplewise_center = TRUE, 
+  samplewise_std_normalization = TRUE, 
+  width_shift_range = 0.1,
+  height_shift_range = 0.1
 ) %>% 
   flow_images_from_directory(
-    "data/spectrograms",  ., seed = 4620, batch_size = 32
+    "../train",  ., seed = 4620, batch_size = 32, target_size = c(640, 480)
+  )
+
+validation_generator <- image_data_generator(
+  samplewise_center = TRUE, 
+  samplewise_std_normalization = TRUE, 
+  width_shift_range = 0.1,
+  height_shift_range = 0.1
+) %>% 
+  flow_images_from_directory(
+    "../validation",  ., seed = 4620, batch_size = 32, target_size = c(640, 480)
   )
 
 # Fit convolutional network -----------------------------------------------
 cnn <- keras_model_sequential()
 
 cnn %>% 
-  layer_conv_2d(filters = 16, kernel_size = 2, activation = "elu", input_shape = c(256, 256, 3)) %>% 
+  layer_conv_2d(filters = 16, kernel_size = 2, activation = "elu", input_shape = c(640, 480, 3)) %>% 
   layer_conv_2d(filters = 16, kernel_size = 2, activation = "elu") %>% 
   layer_max_pooling_2d() %>% 
   layer_dropout(0.2) %>% 
@@ -33,15 +43,24 @@ cnn %>%
   layer_max_pooling_2d() %>% 
   layer_dropout(0.2) %>% 
   layer_flatten() %>% 
-  layer_batch_normalization() %>% 
-  layer_dense(units = 30, activation = "softmax")
+  layer_batch_normalization(trainable = TRUE) %>% 
+  layer_dense(units = 10, activation = "softmax")
 
 cnn %>% 
   compile(
     loss      = "categorical_crossentropy",
     optimizer = optimizer_adam(),
-    metrics   = "accuracy"
+    metrics   = "categorical_accuracy"
   )
 
-cnn %>% 
-  fit_generator(train_generator, 58251 / 32, epochs = 50)
+training_history <- cnn %>% 
+  fit_generator(train_generator, 20000 / 32, epochs = 6)
+
+# Evaluate CNN ------------------------------------------------------------
+eval_results <- cnn %>% 
+  evaluate_generator(validation_generator, steps = 300)
+
+# Save model --------------------------------------------------------------
+save_model_hdf5(cnn, "../convnet.hdf5")
+write_rds(training_history, "../training_history.rds")
+write_rds(eval_results, "../eval_results.rds")
